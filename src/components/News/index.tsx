e501b03bde43851b93c3ec591ee0e2826a2fe29b
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-07-12 15:51:12 -0300

    Add News component

---
+++ src/components/News/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const News: React.FC = () => {
+  return (
+    <Container>
+      <span>Trending topics Brazil</span>
+      <strong>Lerning something new</strong>
+    </Container>
+  );
+};
+
+export default News;
